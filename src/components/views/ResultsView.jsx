b6c2164de533a155e680d11104363399adeb3276
Tony Nguyen <tonynguyen@Tonys-MacBook-Pro.local>
2017-01-14 15:09:22 -0800

    added results

--- src/components/views/ResultsView.jsx
+++ src/components/views/ResultsView.jsx
@@ -4,8 +4,99 @@ class ResultsView extends React.Component {
 
   render() {
     return (
-      <div className="home-view">
-        <h1>Add a Result</h1>
+      <div className="results-view">
+        <ul className="results-list-container">
+          <li>
+            <a href="#">
+              <div className="results-logo">
+                <i className="fa fa-bolt" aria-hidden="true" />
+              </div>
+              <div className="results-stats">
+                <ul>
+                  <li>
+                    <i className="fa fa-money" aria-hidden="true" />
+                    <span>Total Entered:</span>
+                    <span>$300</span>
+                  </li>
+                  <li>
+                    <i className="fa fa-credit-card" aria-hidden="true" />
+                    <span>Total Out:</span>
+                    <span>$500</span>
+                  </li>
+                  <li>
+                    <i className="fa fa-credit-card-alt" aria-hidden="true" />
+                    <span>Profit:</span>
+                    <span>$200</span>
+                  </li>
+                </ul>
+                <div className="results-location">
+                  <i className="fa fa-map-marker" aria-hidden="true" />
+                  <span>Las Vegas, NV</span>
+                </div>
+              </div>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <div className="results-logo">
+                <i className="fa fa-bolt" aria-hidden="true" />
+              </div>
+              <div className="results-stats">
+                <ul>
+                  <li>
+                    <i className="fa fa-money" aria-hidden="true" />
+                    <span>Total Entered:</span>
+                    <span>$300</span>
+                  </li>
+                  <li>
+                    <i className="fa fa-credit-card" aria-hidden="true" />
+                    <span>Total Out:</span>
+                    <span>$500</span>
+                  </li>
+                  <li>
+                    <i className="fa fa-credit-card-alt" aria-hidden="true" />
+                    <span>Profit:</span>
+                    <span>$200</span>
+                  </li>
+                </ul>
+                <div className="results-location">
+                  <i className="fa fa-map-marker" aria-hidden="true" />
+                  <span>Las Vegas, NV</span>
+                </div>
+              </div>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <div className="results-logo">
+                <i className="fa fa-bolt" aria-hidden="true" />
+              </div>
+              <div className="results-stats">
+                <ul>
+                  <li>
+                    <i className="fa fa-money" aria-hidden="true" />
+                    <span>Total Entered:</span>
+                    <span>$300</span>
+                  </li>
+                  <li>
+                    <i className="fa fa-credit-card" aria-hidden="true" />
+                    <span>Total Out:</span>
+                    <span>$500</span>
+                  </li>
+                  <li>
+                    <i className="fa fa-credit-card-alt" aria-hidden="true" />
+                    <span>Profit:</span>
+                    <span>$200</span>
+                  </li>
+                </ul>
+                <div className="results-location">
+                  <i className="fa fa-map-marker" aria-hidden="true" />
+                  <span>Las Vegas, NV</span>
+                </div>
+              </div>
+            </a>
+          </li>
+        </ul>
       </div>
     );
   }
