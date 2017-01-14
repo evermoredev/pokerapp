ad47c91c5ba83420a68b5432be208105d9210d68
Tony Nguyen <tonynguyen@Tonys-MacBook-Pro.local>
2017-01-14 12:52:30 -0800

    added header and footer

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter, Match, Miss, Link } from 'react-router';
 
 // Import view components
 import {
-  AboutView,
   HomeView,
   NotFoundView
 } from './components/views/index.js';
@@ -14,18 +13,44 @@ export default class App extends React.Component {
     return (
       <BrowserRouter>
         <div>
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/about">About</Link></li>
-          </ul>
-
-          <hr/>
+          <nav className="main-nav">
+            <ul>
+              <li>PokerApp</li>
+            </ul>
+          </nav>
 
           <Match exactly pattern="/" component={HomeView} />
-          <Match pattern="/about" component={AboutView} />
 
           {/* If none of those match, then a sibling `Miss` will render. */}
-          <Miss component={NotFoundView}/>
+          <Miss component={NotFoundView} />
+          <footer>
+            <ul>
+              <li>
+                <a href="#">
+                  <i className="fa fa-home" aria-hidden="true" />
+                  <span>Home</span>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <i className="fa fa-area-chart" aria-hidden="true" />
+                  <span>Results</span>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <i className="fa fa-bar-chart" aria-hidden="true" />
+                  <span>Charts</span>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <i className="fa fa-bars" aria-hidden="true" />
+                  <span>More</span>
+                </a>
+              </li>
+            </ul>
+          </footer>
         </div>
       </BrowserRouter>
     );
--- src/components/views/index.js
+++ src/components/views/index.js
@@ -1,3 +1,2 @@
 export HomeView     from './HomeView';
-export AboutView    from './AboutView';
 export NotFoundView from './NotFoundView';
