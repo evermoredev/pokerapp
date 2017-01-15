4e01913d121db64f4d43c0a68a23b1266cc20ef8
Nate Brady <Nathan.Brady@yahoo.com>
2017-01-14 16:27:39 -0800

    sample results json

--- src/components/views/ResultsView.jsx
+++ src/components/views/ResultsView.jsx
@@ -1,103 +1,67 @@
 import React from 'react';
+import * as d3 from 'd3';
 
 class ResultsView extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: null
+    }
+  }
+
+  componentWillMount() {
+    d3.json('static/sample_results.json',
+      (err, data) => this.setState({ data }));
+  }
+
+  renderResultsList() {
+    let data = this.state.data.map(d => (
+      <li>
+        <a href="#">
+          <div className="results-logo">
+            {/*<i className="fa fa-bolt" aria-hidden="true" />*/}
+            <img src="static/images/ca-bicycle.jpg" width="100"/>
+          </div>
+          <div className="results-stats">
+            <ul>
+              <li>
+                <i className="fa fa-money" aria-hidden="true" />
+                <span>Total Entered:</span>
+                <span>${d.buyin}</span>
+              </li>
+              <li>
+                <i className="fa fa-credit-card" aria-hidden="true" />
+                <span>Total Out:</span>
+                <span>${d.cashout}</span>
+              </li>
+              <li>
+                <i className="fa fa-credit-card-alt" aria-hidden="true" />
+                <span>Profit:</span>
+                <span>${d.cashout-d.buyin}</span>
+              </li>
+            </ul>
+            <div className="results-location">
+              <i className="fa fa-map-marker" aria-hidden="true" />
+              <span>Las Vegas, NV</span>
+            </div>
+          </div>
+        </a>
+      </li>
+    ));
+
+    return (
+      <ul className="results-list-container">
+        {data}
+      </ul>
+    );
+  }
 
   render() {
     return (
       <div className="results-view">
-        <ul className="results-list-container">
-          <li>
-            <a href="#">
-              <div className="results-logo">
-                {/*<i className="fa fa-bolt" aria-hidden="true" />*/}
-                <img src="static/images/ca-bicycle.jpg" width="100"/>
-              </div>
-              <div className="results-stats">
-                <ul>
-                  <li>
-                    <i className="fa fa-money" aria-hidden="true" />
-                    <span>Total Entered:</span>
-                    <span>$300</span>
-                  </li>
-                  <li>
-                    <i className="fa fa-credit-card" aria-hidden="true" />
-                    <span>Total Out:</span>
-                    <span>$500</span>
-                  </li>
-                  <li>
-                    <i className="fa fa-credit-card-alt" aria-hidden="true" />
-                    <span>Profit:</span>
-                    <span>$200</span>
-                  </li>
-                </ul>
-                <div className="results-location">
-                  <i className="fa fa-map-marker" aria-hidden="true" />
-                  <span>Las Vegas, NV</span>
-                </div>
-              </div>
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <div className="results-logo">
-                <i className="fa fa-bolt" aria-hidden="true" />
-              </div>
-              <div className="results-stats">
-                <ul>
-                  <li>
-                    <i className="fa fa-money" aria-hidden="true" />
-                    <span>Total Entered:</span>
-                    <span>$300</span>
-                  </li>
-                  <li>
-                    <i className="fa fa-credit-card" aria-hidden="true" />
-                    <span>Total Out:</span>
-                    <span>$500</span>
-                  </li>
-                  <li>
-                    <i className="fa fa-credit-card-alt" aria-hidden="true" />
-                    <span>Profit:</span>
-                    <span>$200</span>
-                  </li>
-                </ul>
-                <div className="results-location">
-                  <i className="fa fa-map-marker" aria-hidden="true" />
-                  <span>Las Vegas, NV</span>
-                </div>
-              </div>
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <div className="results-logo">
-                <i className="fa fa-bolt" aria-hidden="true" />
-              </div>
-              <div className="results-stats">
-                <ul>
-                  <li>
-                    <i className="fa fa-money" aria-hidden="true" />
-                    <span>Total Entered:</span>
-                    <span>$300</span>
-                  </li>
-                  <li>
-                    <i className="fa fa-credit-card" aria-hidden="true" />
-                    <span>Total Out:</span>
-                    <span>$500</span>
-                  </li>
-                  <li>
-                    <i className="fa fa-credit-card-alt" aria-hidden="true" />
-                    <span>Profit:</span>
-                    <span>$200</span>
-                  </li>
-                </ul>
-                <div className="results-location">
-                  <i className="fa fa-map-marker" aria-hidden="true" />
-                  <span>Las Vegas, NV</span>
-                </div>
-              </div>
-            </a>
-          </li>
-        </ul>
+        {this.state.data && this.renderResultsList()}
       </div>
     );
   }
